9441ad967c1a281e7d27c33066cb48ad111f8041
nick-ballinger <nballinger+github@gmail.com>
2016-12-08 09:56:47 -0600

    Fix Plugin Loader
    Removed cordova.define wrapper. Added automatically by cordova on plugin/platform add/remove.

--- www/CameraPreview.js
+++ www/CameraPreview.js
@@ -1,4 +1,3 @@
-cordova.define("com.mbppower.camerapreview.CameraPreview", function(require, exports, module) { 
 var argscheck = require('cordova/argscheck'),
 	utils = require('cordova/utils'),
 	exec = require('cordova/exec');
@@ -55,4 +54,3 @@ CameraPreview.disable = function(disable) {
 
 module.exports = CameraPreview;
 
-});
